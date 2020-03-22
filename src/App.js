54c7ff71e7d96cf58c0fd892af63b63481c66016
Alexander Kluev <alex.kluev@ontario.ca>
2020-03-22 00:43:00 -0400

    added wow!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. <b>Does it really work?</b>
+          Edit <code>src/App.js</code> and save to reload. <b>Does it really work?</b>&nbsp;WOW!!!!!
         </p>
         <a
           className="App-link"
